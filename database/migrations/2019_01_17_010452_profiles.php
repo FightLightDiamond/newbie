0b6f17106127a6bdc71a5326b8e381aaab20dd97
cuongpm <cuongpm@Macs-MacBook-Pro-2.local>
2019-07-23 19:32:12 +0700

    b3

---
+++ database/migrations/2019_01_17_010452_profiles.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Profiles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('height')->nullable();
+            $table->unsignedInteger('weight')->nullable();
+            $table->unsignedInteger('round_one')->nullable();
+            $table->unsignedInteger('round_two')->nullable();
+            $table->unsignedInteger('round_three')->nullable();
+            $table->text('image_one')->nullable();
+            $table->text('image_two')->nullable();
+            $table->text('image_three')->nullable();
+            $table->string('website')->nullable();
+            $table->string('hobby')->nullable();
+            $table->string('slogan')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+}
